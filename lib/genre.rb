13f1697e440f065a11f018ded3f2844eeef69514
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-02-23 17:06:09 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,26 @@
+require 'pry'
+
+class Genre 
+  
+attr_accessor :name, :song 
+  
+@@all = []
+
+def initialize(name)
+  @name = name 
+  @@all << self 
+end 
+
+def self.all 
+  @@all 
+end
+
+def songs
+  Song.all
+end
+
+def artists 
+  songs.map {|song| song.artist}
+end
+
+end
